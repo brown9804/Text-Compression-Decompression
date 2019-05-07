bc92c204cb616a3577d6503430755cae377d7329
Belinda Brown <belindabrownr04@gmail.com>
2019-05-07 13:52:38 -0600

    Add files via upload

---
+++ tabulacion_cambios.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+#Writting first part
+archivo = open("Tabla_Cambios.tab", "w")
+
+archivo.write("\n")
+archivo.write("mi                      ~,~,^^- \n")
+archivo.write("no                       -^^,~,~ \n")
+archivo.write("la                      ~^^:~~  \n")
+
+archivo.write("\n")
+archivo.close()
